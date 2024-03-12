7a1ad1c28b2fdc17905bce6874f67ab71c6818ec
argument100 <100blendy@gmail.com>
2024-03-12 23:55:08 +0900

    In the middle of catr

--- catr/src/lib_sample.rs
+++ catr/src/lib_sample.rs
@@ -12,17 +12,18 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
+// --------------------------------------------------
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("catr")
-        .version("0.1")
-        .author("ozo")
-        .about("rust cat")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust cat")
         .arg(
             Arg::with_name("files")
                 .value_name("FILE")
                 .help("Input file(s)")
                 .multiple(true)
-                .default_value("-")
+                .default_value("-"),
         )
         .arg(
             Arg::with_name("number")
@@ -30,14 +31,14 @@ pub fn get_args() -> MyResult<Config> {
                 .long("number")
                 .help("Number lines")
                 .takes_value(false)
-                .conflicts_with("number_nonblank")
+                .conflicts_with("number_nonblank"),
         )
         .arg(
             Arg::with_name("number_nonblank")
                 .short("b")
-                .long("number_nonblank")
-                .help("Number nonblank lines")
-                .takes_value(false)
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
         )
         .get_matches();
 
@@ -48,14 +49,27 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+// --------------------------------------------------
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
-            Err(err) => eprintln!("Faild to open {}: {}", filename, err),
-            Ok(buf_reader) => {
-                for line in buf_reader.lines()  {
-                    let line = line?;
-                    println!("{}", line);
+            Err(e) => eprintln!("{}: {}", filename, e),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
                 }
             }
         }
@@ -63,6 +77,7 @@ pub fn run(config: Config) -> MyResult<()> {
     Ok(())
 }
 
+// --------------------------------------------------
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
